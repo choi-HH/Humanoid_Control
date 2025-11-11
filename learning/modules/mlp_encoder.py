7723d0be8087dbf9d0dfaf522481c02abbf4c49a
choi-HH <tony00602@gmail.com>
2025-11-11 20:22:52 +0900

    edit mlp_encoder.py

--- learning/modules/mlp_encoder.py
+++ learning/modules/mlp_encoder.py
@@ -1,34 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Copyright (c) 2021 ETH Zurich, Nikita Rudin
-
-
+"""
 import torch
 import torch.nn as nn # 신경망 모듈, 레이어, 활성화 함수 등 가져옴
 
@@ -55,17 +25,16 @@ class MLP_Encoder(nn.Module):
         # 1. 입력층 (Input -> Hidden1)
         encoder_layers.append(nn.Linear(self.num_input_dim, hidden_dims[0])) # 첫 번째 은닉층으로 가는 선형 변환 레이어 추가
         encoder_layers.append(activation) # 활성화 함수 추가
-        """
-        의미: 입력 데이터를 서로 다른 가중치로 조합하여 중간 데이터를 만들고, 활성화 함수를 적용하여 비선형성을 부여(선택적 정보 전달)함.
-        """
+
+        # 의미: 입력 데이터를 서로 다른 가중치로 조합하여 중간 데이터를 만들고, 활성화 함수를 적용하여 비선형성을 부여(선택적 정보 전달)함.
 
         # 2. 중간 은닉층들 (Hidden1 -> Hidden2 ... -> LastHidden)
         for l in range(len(hidden_dims) - 1): # 마지막 은닉층에서 출력층으로 가기 전까지
             encoder_layers.append(nn.Linear(hidden_dims[l], hidden_dims[l + 1])) # 선형 변환 레이어 추가
             encoder_layers.append(activation)
-        """
-        의미: 여러 은닉층을 거치면서 점점 더 추상적이고 복잡한 특징들을 학습함.
-        """
+
+        # 의미: 여러 은닉층을 거치면서 점점 더 추상적이고 복잡한 특징들을 학습함.
+
 
         # 3. 출력층 (LastHidden -> Output)
         encoder_layers.append(nn.Linear(hidden_dims[-1], self.num_output_dim))
@@ -77,14 +46,14 @@ class MLP_Encoder(nn.Module):
         # ==========================================================
 
     def forward(self, input_tensor):
-        """
-        ### 입력이 이미 [batch_size, num_input_dim] 형태로 쭉 펴져있다고 가정함.###
-        Args:
-            input_tensor (torch.Tensor): [batch_size, num_input_dim] (예: [b, 660])
 
-        Returns:
-            torch.Tensor: latent_vector [batch_size, num_output_dim] (예: [b, 32])
-        """
+        ### 입력이 이미 [batch_size, num_input_dim] 형태로 쭉 펴져있다고 가정함.###
+        # Args:
+        #     input_tensor (torch.Tensor): [batch_size, num_input_dim] (예: [b, 660])
+
+        # Returns:
+        #     torch.Tensor: latent_vector [batch_size, num_output_dim] (예: [b, 32])
+
         return self.encoder(input_tensor)
 
     # get_activation 헬퍼 함수
@@ -104,3 +73,38 @@ class MLP_Encoder(nn.Module):
         else:
             print("invalid activation function!")
             return None
+"""
+# 위는 tron1 스타일 코딩
+import torch
+import torch.nn as nn # 신경망 모듈, 레이어, 활성화 함수 등 가져옴
+from .utils import create_MLP
+
+# Estimator network
+class MLP_Encoder(nn.Module):
+    def __init__(self,
+                 num_input_dim,  # obs history 차원
+                 num_output_dim, # latent vector 차원
+                 hidden_dims=[256, 256],
+                 activation="elu",
+                 **kwargs): # 추가 인자는 받지 않음
+        super().__init__()
+
+        self.num_input_dim = num_input_dim
+        self.num_output_dim = num_output_dim
+
+        # MLP 구축
+        self.encoder = create_MLP(num_input_dim, num_output_dim, hidden_dims, activation)
+
+        print(f"Encoder MLP: {self.encoder}")
+        # ==========================================================
+
+    def forward(self, input_tensor):
+
+        ### 입력이 이미 [batch_size, num_input_dim] 형태로 쭉 펴져있다고 가정함.###
+        # Args:
+        #     input_tensor (torch.Tensor): [batch_size, num_input_dim]
+
+        # Returns:
+        #     torch.Tensor: latent_vector [batch_size, num_output_dim]
+
+        return self.encoder(input_tensor)
--- learning/modules/utils/neural_net.py
+++ learning/modules/utils/neural_net.py
@@ -8,28 +8,29 @@ def weights_init_(m):
         torch.nn.init.xavier_uniform_(m.weight, gain=1)
         # torch.nn.init.constant_(m.bias, 0)
 
+# Create a Multi-Layer Perceptron
 def create_MLP(num_inputs, num_outputs, hidden_dims, activation,
                dropouts=None):
 
-    activation = get_activation(activation)
+    activation = get_activation(activation) # 활성화 함수 객체 얻기
 
-    if dropouts is None:
-        dropouts = [0]*len(hidden_dims)
+    if dropouts is None: # 기본값 설정
+        dropouts = [0]*len(hidden_dims) # 드롭아웃 비율 0
 
     layers = []
     if not hidden_dims:  # handle no hidden layers
-        add_layer(layers, num_inputs, num_outputs)
+        add_layer(layers, num_inputs, num_outputs) # 출력층만 추가
     else:
-        add_layer(layers, num_inputs, hidden_dims[0], activation, dropouts[0])
-        for i in range(len(hidden_dims)):
-            if i == len(hidden_dims) - 1:
-                add_layer(layers, hidden_dims[i], num_outputs)
+        add_layer(layers, num_inputs, hidden_dims[0], activation, dropouts[0]) # 입력층 -> 첫 은닉층
+        for i in range(len(hidden_dims)): # 은닉층들 순회
+            if i == len(hidden_dims) - 1: # 마지막 은닉층인 경우
+                add_layer(layers, hidden_dims[i], num_outputs) # 출력층 추가
             else:
                 add_layer(layers, hidden_dims[i], hidden_dims[i+1],
-                          activation, dropouts[i+1])
-    return nn.Sequential(*layers)
+                          activation, dropouts[i+1]) # 은닉층들 추가
+    return nn.Sequential(*layers) # 레이어들을 하나의 모듈로 묶어서 반환
 
-
+# 헬퍼 함수: 활성화 함수 객체 반환
 def get_activation(act_name):
     if act_name == "elu":
         return nn.ELU()
@@ -49,7 +50,7 @@ def get_activation(act_name):
         print("invalid activation function!")
         return None
 
-
+# 헬퍼 함수: 레이어 추가
 def add_layer(layer_list, num_inputs, num_outputs, activation=None, dropout=0):
     layer_list.append(nn.Linear(num_inputs, num_outputs))
     if dropout > 0:
